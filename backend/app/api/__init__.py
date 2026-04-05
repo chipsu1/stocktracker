dd6653e6fd1b4aee053301a12a2b174e08a6201d
chipsu1 <90566943+chipsu1@users.noreply.github.com>
2026-04-05 16:34:40 +0200

    Update __init__.py

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import auth, portfolios, prices
-from app.api.routes import auth, portfolios, prices, import_xtb  
+from app.api.routes import auth, portfolios, prices, import_xtb
+
 api_router = APIRouter(prefix="/api")
 api_router.include_router(auth.router)
 api_router.include_router(portfolios.router)
 api_router.include_router(prices.router)
-api_router.include_router(import_xtb.router) 
+api_router.include_router(import_xtb.router)
